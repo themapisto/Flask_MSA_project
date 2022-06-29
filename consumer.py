e72907e8a188e2e6438189b890c3ebbdfb3ad792
MZC01-KOOK <themapisto@naver.com>
2022-06-29 11:16:34 +0900

    Flask consumer.py

--- consumer.py
+++ consumer.py
@@ -10,11 +10,11 @@ channel.queue_declare(queue='main')
 
 
 def callback(channel, method, properties, body):
-    print('Received in admin')
+    print('Received in main')
     print(body)
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+channel.basic_consume(queue='main', on_message_callback=callback)
 
 print('Started Consuming')
 
