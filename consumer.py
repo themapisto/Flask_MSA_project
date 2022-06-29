6d0ac6e94668485860915d7c69d8136acf0c5370
MZC01-KOOK <themapisto@naver.com>
2022-06-29 16:13:56 +0900

    producer.py consumer.py updated/deleted

--- consumer.py
+++ consumer.py
@@ -1,4 +1,6 @@
-import pika
+import pika, json
+
+from Flask_MSA_project.main import Product, db
 
 params = pika.URLParameters('amqps://wjdihkuz:13LRz2uHVsbK80oPLnLmPfwwowPFbdBs@cougar.rmq.cloudamqp.com/wjdihkuz')
 
@@ -11,7 +13,26 @@ channel.queue_declare(queue='main')
 
 def callback(channel, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product= Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
