27db1b1f31325cdaa20b468d1c7dc8b3985a6887
themapisto <themapisto@naver.com>
2024-02-03 15:45:25 +0900

    re

--- main.py
+++ main.py
@@ -1,45 +1,7 @@
-import json
-from dataclasses import dataclass
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import UniqueConstraint
+from flask import Flask
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
-CORS(app)
 
-db = SQLAlchemy(app)
-
-@dataclass
-class Product(db.Model):
-    id: int
-    title: str
-    image: str
-
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    title = db.Column(db.String(200))
-    image = db.Column(db.String(200))
-
-
-class ProductUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
-
-    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
-
-
-
-
-
-@app.route('/api/products')
-def index():
-    data=Product.query.all()
-    print(data)
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/hellosocar", methods=['GET'])
+def hello():
+  return "hello socar"
